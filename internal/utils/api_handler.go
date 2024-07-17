4579a8ecebdc547a5bff7489c83558abb95feff5
kaumnen <dev@kaumnen.com>
2024-07-17 21:52:24 +0200

    add api calls handler

---
+++ internal/utils/api_handler.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func GetReq(endpoint string) string {
+	logger := GetCiprLogger()
+
+	logger.Printf("Calling API endpoint: %s", endpoint)
+	response, err := http.Get(endpoint)
+
+	if err != nil {
+		logger.Println(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	return string(responseData)
+}
