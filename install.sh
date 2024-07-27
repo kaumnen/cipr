32818f5d2eaa4160f9d3a9747e9accf9df3b2604
kaumnen <dev@kaumnen.com>
2024-07-27 18:20:39 +0200

    update install script

--- install.sh
+++ install.sh
@@ -15,7 +15,8 @@ fi
 RELEASE_URL=$(curl -s https://api.github.com/repos/kaumnen/cipr/releases/latest \
     | grep "browser_download_url.*$BINARY_NAME" \
     | cut -d : -f 2,3 \
-    | tr -d \")
+    | tr -d \" \
+    | xargs)
 
 if [ -z "$RELEASE_URL" ]; then
     echo "Failed to fetch the latest release URL. Please check your internet connection and try again."
@@ -26,8 +27,13 @@ echo "Downloading the latest release from: $RELEASE_URL"
 
 curl -L -o cipr "$RELEASE_URL"
 
+INSTALL_DIR="$HOME/.cipr/bin"
+mkdir -p "$INSTALL_DIR"
 chmod +x cipr
+mv cipr "$INSTALL_DIR"
 
-sudo mv cipr /usr/local/bin/
-
-echo "Installation complete. You can now use the 'cipr' command."
+echo "Installation complete. The 'cipr' command has been installed to $INSTALL_DIR"
+echo "Please ensure $INSTALL_DIR is in your PATH."
+echo "You may need to add the following line to your shell configuration file (e.g., .bashrc, .zshrc):"
+echo "export PATH=\"\$HOME/.cipr/bin:\$PATH\""
+echo "After adding the line, restart your terminal or run 'source ~/.bashrc' (or the appropriate config file) to update your PATH."
