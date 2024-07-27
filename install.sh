8c88c7cb22f926efb2d74c6865b4fd56d97bc12b
kaumnen <dev@kaumnen.com>
2024-07-27 18:11:22 +0200

    fix bug in install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,8 @@ fi
 RELEASE_URL=$(curl -s https://api.github.com/repos/kaumnen/cipr/releases/latest \
     | grep "browser_download_url.*$BINARY_NAME" \
     | cut -d : -f 2,3 \
-    | tr -d \")
+    | tr -d \" \
+    | xargs)
 
 if [ -z "$RELEASE_URL" ]; then
     echo "Failed to fetch the latest release URL. Please check your internet connection and try again."
