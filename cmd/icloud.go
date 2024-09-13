5135ccf30a49de98de41290ffa57ba52c08fd49c
kaumnen <dev@kaumnen.com>
2024-09-13 22:20:45 +0200

    add support for multiple search params

--- cmd/icloud.go
+++ cmd/icloud.go
@@ -8,9 +8,9 @@ import (
 var (
 	icloudIPv4Flag          bool
 	icloudIPv6Flag          bool
-	icloudFilterCountryFlag string
-	iCloudFilterStateFlag   string
-	iCloudFilterCityFlag    string
+	icloudFilterCountryFlag []string
+	iCloudFilterStateFlag   []string
+	iCloudFilterCityFlag    []string
 )
 
 var icloudCmd = &cobra.Command{
@@ -30,8 +30,8 @@ var icloudCmd = &cobra.Command{
 func init() {
 	icloudCmd.Flags().BoolVar(&icloudIPv4Flag, "ipv4", false, "Get only IPv4 ranges")
 	icloudCmd.Flags().BoolVar(&icloudIPv6Flag, "ipv6", false, "Get only IPv6 ranges")
-	icloudCmd.Flags().StringVar(&icloudFilterCountryFlag, "filter-country", "", "Filter results by country")
-	icloudCmd.Flags().StringVar(&iCloudFilterStateFlag, "filter-state", "", "Filter results by state")
-	icloudCmd.Flags().StringVar(&iCloudFilterCityFlag, "filter-city", "", "Filter results by city (use quotes for names with spaces, e.g. \"New York\")")
+	icloudCmd.Flags().StringSliceVar(&icloudFilterCountryFlag, "filter-country", []string{}, "Filter results by country")
+	icloudCmd.Flags().StringSliceVar(&iCloudFilterStateFlag, "filter-state", []string{}, "Filter results by state")
+	icloudCmd.Flags().StringSliceVar(&iCloudFilterCityFlag, "filter-city", []string{}, "Filter results by city (use quotes for names with spaces, e.g. \"New York\")")
 	rootCmd.AddCommand(icloudCmd)
 }
--- internal/icloud/logic.go
+++ internal/icloud/logic.go
@@ -15,7 +15,7 @@ type IPRange struct {
 	City    string
 }
 
-func GetIPRanges(ipType, filterCountry, filterState, filterCity string) {
+func GetIPRanges(ipType string, filterCountry, filterState, filterCity []string) {
 	ip_ranges_data := loadData()
 	readyIPs := filtrateIPRanges(ip_ranges_data, ipType, filterCountry, filterState, filterCity)
 	fmt.Println(readyIPs)
@@ -46,17 +46,26 @@ func loadData() []IPRange {
 	return ipRanges
 }
 
-func filtrateIPRanges(ipRanges []IPRange, ipType, filterCountry, filterState, filterCity string) []IPRange {
+func filtrateIPRanges(ipRanges []IPRange, ipType string, filterCountries, filterStates, filterCities []string) []IPRange {
 	var readyIPs []IPRange
 
 	for _, ipRange := range ipRanges {
 		if (ipType == "ipv4" && strings.Contains(ipRange.IPRange, ".")) || (ipType == "ipv6" && strings.Contains(ipRange.IPRange, ":")) {
-			if (filterCountry == "" || strings.EqualFold(ipRange.Country, filterCountry)) &&
-				(filterState == "" || strings.EqualFold(ipRange.State, filterState)) &&
-				(filterCity == "" || strings.EqualFold(ipRange.City, filterCity)) {
+			if (len(filterCountries) == 0 || containsIgnoreCase(filterCountries, ipRange.Country)) &&
+				(len(filterStates) == 0 || containsIgnoreCase(filterStates, ipRange.State)) &&
+				(len(filterCities) == 0 || containsIgnoreCase(filterCities, ipRange.City)) {
 				readyIPs = append(readyIPs, ipRange)
 			}
 		}
 	}
 	return readyIPs
+}
+
+func containsIgnoreCase(slice []string, item string) bool {
+	for _, s := range slice {
+		if strings.EqualFold(s, item) {
+			return true
+		}
+	}
+	return false
 }
