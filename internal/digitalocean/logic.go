26cb9658be600d64237b5b595cfea0c0dc7e717d
kaumnen <dev@kaumnen.com>
2024-10-19 21:33:49 +0200

    align raw data handler with digital ocean provider

--- internal/digitalocean/logic.go
+++ internal/digitalocean/logic.go
@@ -36,9 +36,13 @@ func GetIPRanges(config Config) {
 }
 
 func loadData() []IPRange {
-	rawData := utils.GetRawData("https://digitalocean.com/geo/google.csv")
+	rawData := utils.GetRawData("digitalocean")
+
 	r := csv.NewReader(strings.NewReader(rawData))
+	r.FieldsPerRecord = -1
+
 	records, err := r.ReadAll()
+
 	if err != nil {
 		fmt.Println("Error:", err)
 		return nil
@@ -46,16 +50,25 @@ func loadData() []IPRange {
 
 	var ipRanges []IPRange
 	for _, record := range records {
-		if len(record) < 5 {
-			continue
+
+		ipRange := IPRange{}
+
+		if len(record) > 0 {
+			ipRange.IPRange = strings.TrimSpace(record[0])
 		}
-		ipRange := IPRange{
-			IPRange: record[0],
-			Country: record[1],
-			Region:  record[2],
-			City:    record[3],
-			Zip:     record[4],
+		if len(record) > 1 {
+			ipRange.Country = strings.TrimSpace(record[1])
 		}
+		if len(record) > 2 {
+			ipRange.Region = strings.TrimSpace(record[2])
+		}
+		if len(record) > 3 {
+			ipRange.City = strings.TrimSpace(record[3])
+		}
+		if len(record) > 4 {
+			ipRange.Zip = strings.TrimSpace(record[4])
+		}
+
 		ipRanges = append(ipRanges, ipRange)
 	}
 	return ipRanges
