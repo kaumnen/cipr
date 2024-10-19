e1c03decde3a476791c64d2b5453a8b8967bfee0
kaumnen <dev@kaumnen.com>
2024-10-19 21:01:34 +0200

    update raw data handler util to use viper config file

--- internal/utils/raw_data_handler.go
+++ internal/utils/raw_data_handler.go
@@ -1,14 +1,63 @@
 package utils
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+
+	"github.com/spf13/viper"
 )
 
-func GetRawData(endpoint string) string {
+func GetRawData(provider string) string {
+	endpointURL := viper.GetString(provider + "_endpoint")
+	localFile := viper.GetString(provider + "_local_file")
+
+	var ipRanges string
+	var err error
+
+	if localFile != "" {
+
+		fmt.Println("Fetching IP ranges from local file:", localFile)
+		ipRanges, err = loadFromFile(localFile)
+
+		if err != nil {
+			fmt.Println("Error reading local file:", err)
+			os.Exit(1)
+
+		}
+	} else {
+		if endpointURL == "" {
+			fmt.Println("No endpoint URL or local file specified.")
+			os.Exit(1)
+		}
+
+		fmt.Println("Fetching IP ranges from endpoint:", endpointURL)
+
+		ipRanges, err = loadFromEndpoint(endpointURL)
+
+		if err != nil {
+			fmt.Println("Error fetching from endpoint:", err)
+			os.Exit(1)
+		}
+	}
+
+	return ipRanges
+}
+
+func loadFromFile(filePath string) (string, error) {
+	data, err := os.ReadFile(filePath)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
+
+func loadFromEndpoint(url string) (string, error) {
 	logger := GetCiprLogger()
-	response, err := http.Get(endpoint)
+	response, err := http.Get(url)
 
 	if err != nil {
 		logger.Println(err.Error())
@@ -21,5 +70,5 @@ func GetRawData(endpoint string) string {
 		logger.Fatal(err)
 	}
 
-	return string(responseData)
+	return string(responseData), nil
 }
